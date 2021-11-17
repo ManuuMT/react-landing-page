d56ffdd1418ffa9d437298782657d58b8e98333b
Jesús Cano <jcanort@gmail.com>
2021-11-17 20:22:37 +0000

    Creating Main Component

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,8 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+import Main from "../component/main.jsx";
+
 //create your first component
 const Home = () => {
 	return (
@@ -19,6 +21,7 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+			<Main />
 		</div>
 	);
 };
---
+++ src/js/component/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+//include images into your bundle
+//import heroPic from "../../img/hero-pic.png";
+
+//create your first component
+const Main = () => {
+	return (
+		<div className="container-fluid bg-dark">
+			<div className="container-fluid">
+				<h3 id="intro-name">Hola, Soy Manu Maldonado</h3>
+				<h1 id="intro-title" className="text-white">
+					TE AYUDO A CRECER TU NEGOCIO CON TECNOLOGÍAS DE PRIMERA.
+				</h1>
+			</div>
+		</div>
+
+		//Main-background
+		//Main-title
+		//Main-bottom?
+	);
+};
+
+export default Main;
