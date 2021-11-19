fb79f9a6bd4494147084f1c45ccd4c4a687dcb57
Jesús Cano <jcanort@gmail.com>
2021-11-19 20:05:28 +0000

    CreatingStructure, Title and main Images

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,18 @@
 import React from "react";
 
-//include images into your bundle
+//Components
+import Main from "../component/main.jsx";
+
+//Resources
 import rigoImage from "../../img/rigo-baby.jpg";
 
-import Main from "../component/main.jsx";
+//Styles
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
 			<Main />
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
 		</div>
 	);
 };
--- src/js/component/main.jsx
+++ src/js/component/main.jsx
@@ -1,26 +1,45 @@
 import React from "react";
 
-//include images into your bundle
-//import heroPic from "../../img/hero-pic.png";
+//import PropTypes from "prop-types"
+//importar estilos main.scss
 
 //create your first component
 const Main = () => {
 	return (
-		<div className="container-fluid bg-black">
-			<div className="container container-main">
-				<div className="container-text">
-					<h3 id="intro-name">Hola, Soy Manu Maldonado</h3>
-					<h1 id="intro-title" className="text-white">
-						TE AYUDO A CRECER TU NEGOCIO CON TECNOLOGÍAS DE PRIMERA.
-					</h1>
+		<section className="container-fluid bg-black">
+			<div className="container d-flex align-items-center container-content">
+				<div className="container container-text">
+					<div className="col-md-8">
+						<h3 id="intro-name">Hola, Soy Manu Maldonado</h3>
+						<h1 id="intro-title" className="text-white">
+							Te ayudo a crecer tu negocio con tecnologías de
+							primera.
+						</h1>
+					</div>
+					<div className="scroll-button mt-4">
+						<a href="#" className="scroll-button-link">
+							Haz scroll para conocerme
+						</a>
+					</div>
 				</div>
-				<div></div>
 			</div>
-		</div>
-
-		//Main-background
-		//Main-title
+			<div className="container">
+				<div className="grid-points"></div>
+				<div className="hero-pic"></div>
+			</div>
+		</section>
 	);
 };
+
+/* Para asegurar que nunca haya 
+errores en el componente, le decimos a Javascript ñ
+el tipo que le pasamos
+{props.tilte}, {props.description}, {props.buttonTitle}...
+Card.propTypes = {
+	title: PropTypes.string.isRequired;
+	description: PropTypes.string;
+	buttonTitle: PropTypes.string;
+};
+*/
 
 export default Main;
