1dcff80f86ff73c35668b081a1d576b2a4d6de9f
ManuuMT <ema14mt@gmail.com>
2021-11-22 01:30:19 +0000

    Creating Main Body

--- src/js/component/NavItemSocial.jsx
+++ src/js/component/NavItemSocial.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const NavItemSocial = prop => {
 	return (
@@ -8,6 +9,10 @@ const NavItemSocial = prop => {
 			</a>
 		</li>
 	);
+};
+
+NavItemSocial.propTypes = {
+	network: PropTypes.string
 };
 
 export default NavItemSocial;
