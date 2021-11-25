f763ee15dae5e6ad57b6bea2d2d389d2caa71535
Jesús Cano <jcanort@gmail.com>
2021-11-25 22:05:17 +0000

    Cleaning Intro conflicts

--- src/js/component/FooterCopy.jsx
+++ src/js/component/FooterCopy.jsx
@@ -2,15 +2,14 @@ import React from "react";
 
 const FooterCopy = () => {
 	return (
-		<div className="container-fluid">
-			<div className="container d-flex flex-row">
-				<div className="container d-flex flex-row">
-					<p>Copyright</p>
-					<p>Diseñado por...</p>
+		<div className="container-fluid py-5">
+			<div className="container d-flex flex-row copy-text">
+				<div className="d-flex flex-row">
+					<p>© Copyright Manu Maldonado 2021</p>
+					<p className="mx-3">
+						Diseñado por <span>Manu Maldonado</span>
+					</p>
 				</div>
-				<button type="button" className="btn btn-primary">
-					Arrow Up
-				</button>
 			</div>
 		</div>
 	);
--- src/js/component/Intro.jsx
+++ src/js/component/Intro.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 
+import heroPic from "../../img/hero-pic-modified.jpg";
+import gridDots from "../../img/pattern-dot-grid.svg";
+
 const Intro = () => {
 	return (
-		<section className="container-fluid bg-black">
-			<div className="container d-flex align-items-center container-content">
-				<div className="container-fluid container-text">
-					<div className="col-xl-6 col-lg-6 col-md-6">
+		<section className="container-fluid bg-black d-flex align-items-center">
+			<div className="container">
+				{/* Componente 1 */}
+				<div className="row">
+					<div className="col-lg-6 col-md-12 col-sm-12">
 						<h3 id="intro-name">Hola, Soy Manu Maldonado</h3>
-						<h1 id="intro-title" className="h1 text-white">
+						<h1 id="intro-title">
 							Te ayudo a crecer tu negocio con tecnologías de
 							primera.
 						</h1>
-					</div>
-					<div className="scroll-button mt-4">
-						<a href="#" className="scroll-button-link">
-							Haz scroll para conocerme
-						</a>
+						<div className="d-flex flex-row align-items-center">
+							<a href="#" className="scroll-button">
+								Haz scroll para conocerme
+							</a>
+							<div className="line-intro mx-2"></div>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="container">
-				<div className="grid-points"></div>
-				<div className="hero-pic"></div>
+				{/* Componente 2 */}
+				<div className="container">
+					<img
+						src={heroPic}
+						className="hero-pic img-fluid"
+						alt="hero"
+					/>
+					{/* <img
+						src={gridDots}
+						className="dots-pic img-fluid"
+						alt="Dots"
+					/> */}
+				</div>
 			</div>
 		</section>
 	);
